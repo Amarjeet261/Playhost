1b5a544ce7d8801095aab5f401dd86adaa839ea9
Amarjeet261 <amarjeetrajput326@gmail.com>
2025-06-15 15:01:15 +0530

    6th change

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/Components/Footer/Footer";
 import Navbar from "@/Components/Navbar/Navbar";
 import { RegisterForm } from "@/Components/Register/Register";
 
-export default Register function (){
+export default  function Register(){
     return(
         <section className="">
             <Navbar/>
